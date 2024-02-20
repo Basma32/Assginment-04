59c6fbdd3664b49bf30102a68a78ebb2aff80307
unknown <basmaakter08@gmail.com>
2024-02-20 14:31:14 +0600

    first commit

---
+++ Nested.js
@@ -0,0 +1,13 @@
+var cookiesprice = 90;
+var kekprice = 50;
+var sevenupprice = 25;
+var myBudget = 40;
+
+if(cookiesprice < mybudget){
+    console.log("sokale ami cookies khabo")
+}
+else if(kekprice < myBudget){
+    console.log('cha diye kek khabo')
+}else if(sevenupprice < myBudget){
+    console.log('cha na paile ami sevenup khabo')
+}
---
+++ if,else.js
@@ -0,0 +1,12 @@
+var gotjob = false;
+var saveMoney = 300000;
+var hasflat = true;
+var hasHouse = true;
+
+
+if((gotjob == true || saveMoney > 250000) && (hasflat == true|| hasHouse == true)){
+    console.log('cholo biya kori!!');
+}
+else{
+    console.log('tor kopale biya nai');
+}
